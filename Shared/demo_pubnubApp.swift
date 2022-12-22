844cf2292e091e1174fc6bfed4645ac7882342ce
adarsh <adarsh0ranjan@gmail.com>
2022-12-22 10:27:26 +0530

    Initial Commit

---
+++ Shared/demo_pubnubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_pubnubApp.swift
+//  Shared
+//
+//  Created by Roro Solutions on 22/12/22.
+//
+
+import SwiftUI
+
+@main
+struct demo_pubnubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
